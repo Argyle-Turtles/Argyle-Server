4926c0797394fe4a0b57a14b9630f0c54e6b9836
Trey Borro <tab9204@g.rit.edu>
2016-03-31 10:31:56 -0400

    suck it peter and git

--- src/client/tablet/index.js
+++ src/client/tablet/index.js
@@ -2,14 +2,23 @@ import m from 'mithril';
 
 import { head } from '../components';
 // client entry file
+const accessToken = "BQCTArAfX5YwDrC3Ti36xci8XMKQ-HBDY7jOEJbiNw4qRSuFoMLNdDNPuQS8oZj7gj36Zs75fmcDpU7tfgCHfKZm3Z9ZilxD9q6pFoBNMPkyUil5JDftGZMbETGhS2_yzLNZkTJJwlhOB6qWpNJxqj70M5RZHYh8lq2jS06k6YKlU1efR3LF6z5mGEAqhzh7ClKolEwqrYwbPJffI1bG1Bbcm58YBZmB6rw79Fbg0Ku8NCR-UbCE0A"
+const spotify = {
+    getPlaylist: function(){
+        return m.request({method:"GET", url:"https://api.spotify.com/v1/users/{user_id}/playlists/{playlist_id}",headers:{
+                         'Authorization': 'Bearer ' + accessToken,
+                         'Content-type': 'application/json'}});
+    }
+}
 
-export const stageOne = {
+export const stageOne = { 
 	view: function(){
 		return m("html",[
-							head,
+				head,
               m("body",[
                 m(".stageOne",[
                 	m(".banner",[
+                        m("button",{onclick:function(e){document.location.href='https://accounts.spotify.com:/authorize?client_id=f81d5574fd5b4b57b517ba20081f3235&response_type=token&redirect_uri=http://localhost:3000/#tablet/&scope=playlist-modify-private playlist-modify-public&show_dialog=false';}},"Authorize"),
                 		m("div","Enter Your Code")
                 	]),
                 	m(".content",[
@@ -24,6 +33,21 @@ export const stageOne = {
               ]),
             ]);
 	}
+};
+
+export const myMix = {
+    view: function(){
+        return m("html",[
+          head,
+                  m("body",[
+                    m(".myMix",[
+                      m(".graph","Graph goes here"),
+                      m(".songList","Song list")
+                      ]),
+                    ]),
+                ]);
+    }
+
 };
 
 export const stageTwo = {
