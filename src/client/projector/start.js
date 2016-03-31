4c15aa325b02be7c94ca3dd44c614198336121ce
Peter Gyory <ptg3662@rit.edu>
2016-03-30 23:01:17 -0400

    Trey I added a lint script but it's fucked

--- src/client/projector/start.js
+++ src/client/projector/start.js
@@ -1,27 +1,42 @@
+import { map } from 'ramda';
 import m, { prop } from 'mithril';
+
 import { head } from '../components';
 
 // VIEW MODEL
 const vm = {
-  init: () =>
-    vm.bandName = prop('Captain Trey and the Peppy Peters'),
+  init: () => {
+    vm.bandName = prop('Captain Trey and the Peppy Peters');
+    vm.primaryGenre = prop('Rock');
+    vm.subGenres = prop(['Funck Rock', 'Alt Rock', 'Funk Metal']);
+  },
 };
 
 // VIEW
-const view = data =>
+const view = () =>
   m('html', [
-          head,
-          m('body', [
-            m('#pageone', [
-              bandInfo(data),
-            ]),
-          ]),
-        ]);
+    head,
+    m('body', [
+      m('#pageone', [
+        bandInfo(),
+      ]),
+    ]),
+  ]);
 
 // COMPONENTS
-const bandInfo = ()=>
-  m('.bandInfo', [
+const subGenres = () =>
+  map(genre => m('span[class=sub-genre]', genre), vm.subGenres());
+
+const bandGenre = () =>
+  m('.band-genre', [
+    m('h2', vm.primaryGenre()),
+    subGenres(),
+  ]);
+
+const bandInfo = () =>
+  m('.band-info', [
     m('h1', vm.bandName()),
+    bandGenre(),
   ]);
 
 // CONTROLER
