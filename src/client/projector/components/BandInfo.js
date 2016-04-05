24d8f45d0a33edd557fdc08f16e86c2a96c2ef25
Peter Gyory <ptg3662@rit.edu>
2016-04-05 19:08:47 -0400

    something i'm sure

--- src/client/projector/components/BandInfo.js
+++ src/client/projector/components/BandInfo.js
@@ -1,14 +1,15 @@
 import { map } from 'ramda';
-// <h3 className="main-genre title column">{args.primaryGenre}</h3>
 
 // COMPONENTS
 const view = (ctrl, args) =>
   <div className="band-info">
     <h1 className="band-name title is-1">{args.bandName}</h1>
-    <div className="genre container columns is-multiline">
-      <h3 className="main-genre title column is-half is-offset-5">{args.primaryGenre}</h3>
-      <div className="sub-genres columns column is-half is-offset-6">
-       {map(genre => <span className="sub-genre column is-3 is-text-left">{genre}</span>, args.subGenres)}
+    <div className="genre container">
+      <h3 className="main-genre title is-3">{args.primaryGenre}</h3>
+      <div className="sub-genres">
+       {map(genre =>
+          <span className="sub-genre column is-3 is-text-center">{genre}</span>,
+          args.subGenres)}
       </div>
     </div>
     <div className="columns band-history">
