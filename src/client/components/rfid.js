e0d04ef56cfae778bfd7bacef58bccaecbee2fec
Peter Gyory <ptg3662@rit.edu>
2016-04-12 16:27:58 -0400

    Add rfid file

---
+++ src/client/components/rfid.js
@@ -0,0 +1,13 @@
+
+const controller = () => {
+
+};
+
+const view = () =>
+  <div className="hidden">
+    <link href="./build/css/main.css" rel="stylesheet"></link>
+  </div>;
+
+export default {
+  view,
+};
