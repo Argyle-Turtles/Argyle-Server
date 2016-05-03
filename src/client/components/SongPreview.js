76d0fc4a6fea4e13423e095535ad7dc0b3746f32
Peter Gyory <ptg3662@rit.edu>
2016-05-03 14:00:52 -0400

    Fix src switching playing

--- src/client/components/SongPreview.js
+++ src/client/components/SongPreview.js
@@ -1,28 +1,24 @@
 import m from 'mithril';
 
-// mithril stuff
-const vm = {
-  init: () =>
-    vm.source = m.prop('https://p.scdn.co/mp3-preview/934da7155ec15deb326635d69d050543ecbee2b4'),
-};
+const isAudioReady = () => document.querySelector('#preview-audio').readyState === 4;
 
 /** Plays current audio */
-const playAudio = () => document.querySelector('#preview-audio').play();
+const playAudio = () => {
+  const element = document.querySelector('#preview-audio');
+  if (isAudioReady()) element.play();
+  else element.onloadeddata = () => element.play();
+};
 
 /** Pauses current audio */
 const pauseAudio = () => document.querySelector('#preview-audio').pause();
 
 /** Sets audio source */
-const setAudioSource = newUrl => vm.source(newUrl);
+const setAudioSource = newUrl => document.querySelector('#preview-audio').src = newUrl;
 
-const view = () => <audio id="preview-audio" src={vm.source()}></audio>;
-
-const controller = () => vm.init();
+const view = () => <audio id="preview-audio" src="https://p.scdn.co/mp3-preview/934da7155ec15deb326635d69d050543ecbee2b4"></audio>;
 
 export default {
-  vm,
   view,
-  controller,
   playAudio,
   pauseAudio,
   setAudioSource,
--- src/client/projector/selection.js
+++ src/client/projector/selection.js
@@ -92,6 +92,9 @@ const vm = {
 
 const pickACard = id => () => {
   [0, 1, 2].map(index => id === index ? selectCard(index)() : deselectCard(index)());
+
+  SongPreview.setAudioSource('https://p.scdn.co/mp3-preview/eab1dc25e61631b135a84bbc206d63604494e199');
+  SongPreview.playAudio();
 };
 
 // VIEWS
